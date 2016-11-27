2714829a8782f61e3bfc09b892a3264cd7e95ea6
Surbhi Aggarwal <saggarw9@asu.edu>
2016-11-27 16:24:13 -0700

    header and leftMenu jsp

--- src/main/java/course/dv/webmd/controller/MainController.java
+++ src/main/java/course/dv/webmd/controller/MainController.java
@@ -14,10 +14,18 @@ public class MainController {
 	@RequestMapping(value = { "/", "/welcome**","/logout" }, method = RequestMethod.GET)
 	public ModelAndView defaultPage() {
 		ModelAndView model = new ModelAndView();
-		model.setViewName("login");
+		model.setViewName("welcome");
 		return model;
 
 	}
+	@RequestMapping(value = "topicsByPopularity", method = RequestMethod.GET)
+	public ModelAndView topicsByPopularity() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("topicsByPopularity");
+		return model;
+
+	}
+	
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public ModelAndView defaultPageAfterLogin(HttpServletRequest request,@RequestParam("username") String username,@RequestParam("password") String password) {
 		request.getSession().setAttribute("user", username);
